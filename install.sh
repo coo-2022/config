80024cd7a1ddf12489a2fd152d52b048371daf90
niuhaoxin <niuhaoxin@huawei.com>
2022-04-14 19:53:01 +0800

    支持自动安装nvim

--- install.sh
+++ install.sh
@@ -5,34 +5,7 @@ PROXY=""
 
 source ./util.sh
 
-FILE_BASHRC=""
-FILE_NHXENV=""
-FILE_NHXALIAS=""
-FILE_CURLRC=""
-
-function setup_env()
-{
-    _print_event "Begin setup env."
-    if [ ! -z ${FILE_NHXENV} ]; then
-        echo "${FILE_NHXENV}" >> ~/.nhxenv
-    fi
-
-    if [ ! -z ${FILE_NHXALIAS} ]; then
-        echo "${FILE_NHXALIAS}" >> ~/.nhxalias
-    fi
-
-    if [ ! -z ${FILE_BASHRC} ]; then
-        echo "${FILE_BASHRC}" >> ~/.bashrc
-    fi
-
-    if [ ! -z ${FILE_CURLRC} ]; then
-        echo "${FILE_CURLRC}" >> ~/.curlrc
-    fi
-
-    souce ~/.bashrc
-
-    return 0
-}
+PROXY=
 
 function setup_gcc()
 {
@@ -106,28 +79,121 @@ function check_result()
     _print_event "Check env setup ok"
 }
 
-function setup_nvim()
+function download_nvim()
+{
+    return 0
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    if [ $? -ne 0 ]; then
+        _print_error "Download nvim failed."
+        return 1
+    fi
+
+    _print_event "Install binary."
+    mkdir -p /opt/nvim/
+    chmod +x nvim.appimage
+    cp nvim.appimage /opt/nvim/
+    ln -s /opt/nvim/nvim.appimage /usr/bin/nvim
+
+    return 0
+}
+
+function install_nvim()
 {
     _print_event "Set up nvim."
     which nvim
-    if [ $? -ne 0 ]; then
+    if [ $? -eq 0 ]; then
+        _print_event "Nvim is already installed."
+        return 0
     fi
-    mkdir -p ~/.config/nvim/
-    cp ./nvim/init.vim ~/.config/nvim/init.vim
+
+    _print_event "Download nvim..."
+    download_nvim
+
+    _print_event "Install init.vim"
+    mkdir -p ~/.config/nvim/ 
+    cp nvim/init.vim  ~/.config/nvim/
+
+    return 0
+}
+
+function setup_proxy()
+{
+    apt-get update
+    apt install curl -y
+
+    echo "--insecure" >> ~/.curlrc
+    echo "proxy = ${PROXY}" >> ~/.curlrc
+    return 0
+}
+
+function install_git()
+{
+    which git
+    if [ $? -eq 0 ]; then
+        _print_event "Git is already installed."
+        return 0
+    fi
+
+    echo "[http \"https://github.com\"]" >> ~/.gitconfig
+    echo "proxy = \"${PROXY}\"" >> ~/.gitconfig
+    echo "sslVerify = false" >> ~/.gitconfig
+
+    apt install git -y
+}
+
+function install_nvim_plug()
+{
+    _print_event "Download vim-plug"
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    export http_proxy="${PROXY}"
+    export https_proxy="${PROXY}"
+    nvim -c "PlugInstall" -c "q" -c "q"
+
+    return 0
+}
+
+function install_coc_depedency()
+{
+    which node
+    if [ $? -eq 0 ]; then
+        _print_event "Node is already installed."
+        return 0
+    fi
+
+    VERSION=v16.14.2
+    DISTRO=linux-x64
+
+    curl -LO https://nodejs.org/dist/${VERSION}/node-${VERSION}-${DISTRO}.tar.xz
+    mkdir -p /opt/nodejs/
+    tar -xJvf node-${VERSION}-${DISTRO}.tar.xz -C /opt/nodejs/
+    # 支持重入
+    echo "export PATH=\"/opt/nodejs/node-$VERSION-$DISTRO/bin:\$PATH\"" >> ~/.profile
+    . ~/.profile
+
+    return 0
+}
+
+function install_clangd()
+{
 }
 
 function main()
 {
-    setup_env
-    if [ $? -ne 0 ]; then
-        _print_error "setup env failed."
-        exit 1
-    fi
-    setup_software
-    if [ $? -ne 0 ]; then
-        _print_error "setup software failed."
-        exit 1
-    fi
+    _print_event "Setup proxy"
+    setup_proxy
+
+    _print_event "Install git"
+    install_git
+
+    _print_event "Install nvim"
+    install_nvim
+
+    _print_event "Install coc depedency"
+    install_coc_depedency
+
+    _print_event "Install nvim plug"
+    install_nvim_plug
 
     exit 0
 }
